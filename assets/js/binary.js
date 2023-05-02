6729f0fb424167f03341504ceaae1c581c1270ce
Joey Wunderlich <jwunderl@users.noreply.github.com>
2023-05-02 12:53:48 -0700

    0.0.2

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.30/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.1","target":"arcade","targetVersion":"1.12.30","repo":"jwunderl/pxt-roman-numerals"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.30/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.2","target":"arcade","targetVersion":"1.12.30","repo":"jwunderl/pxt-roman-numerals"}
 // total=1247290 new=5.85% cached=88.48% other=5.67%
 (function (ectx) {
 'use strict';
@@ -429,7 +429,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P44519(s) {
+function _main___P44684(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -831,13 +831,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P44519.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P44519.continuations = [  ]
+_main___P44684.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P44684.continuations = [  ]
 
-function _main___P44519_mk(s) {
+function _main___P44684_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P44519, depth: s.depth + 1,
+        parent: s, fn: _main___P44684, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42295,5 +42295,5 @@ const sprites_BaseSpriteSayRenderer__C20241_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P44519
+return _main___P44684
 })
